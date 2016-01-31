8eb47c868f8776cc846e58f12ef66e489d92b33d
mbird <ado.be@posteo.de>
2016-01-31 16:58:05 +0100

    di - dah

--- beep.py
+++ beep.py
@@ -1,4 +1,21 @@
 import winsound
+import time
+
+# Create beep sound
 Freq = 2500 # Set Frequency To 2500 Hertz
-Dur = 500 # Set Duration To 1000 ms == 1 second
+Dur = 1000 # Set Duration To 1000 ms == 1 second
 winsound.Beep(Freq,Dur)
+time.sleep(0.1) # wait for 0.1 seconds
+
+
+# create di
+Freq = 2500 # Set Frequency To 2500 Hertz
+di = 200 # Set Duration To 200 ms
+winsound.Beep(Freq,di)
+time.sleep(0.2) # wait for 0.2 seconds
+
+# create dah
+Freq = 2500 # Set Frequency To 2500 Hertz
+dah = 600 # Set Duration To 600 ms
+winsound.Beep(Freq,dah)
+time.sleep(0.2) # wait for 0.2 seconds
